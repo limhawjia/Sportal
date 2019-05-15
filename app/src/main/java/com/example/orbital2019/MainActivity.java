d7c6a1886f563408efec372e112c7fb653d52719
weiiijiiie <h_weijie@yahoo.com>
2019-05-16 07:55:17 +0800

    Reverted Test Commit

--- app/src/main/java/com/example/orbital2019/MainActivity.java
+++ app/src/main/java/com/example/orbital2019/MainActivity.java
@@ -7,7 +7,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("test");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
