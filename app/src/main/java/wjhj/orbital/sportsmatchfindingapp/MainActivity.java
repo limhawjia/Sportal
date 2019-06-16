435617e3ee2bee0d7fe87c021af8c3511f4e4d68
Haw <hawjiaa@gmail.com>
2019-06-17 00:06:19 +0800

    Added user and game class to app, together with basic methods to add and get games/users. Database related classes are all under the database package with Data being the main class for interacting with the database. Also added a UserState under new auth package to keep track of the current user.

--- app/src/main/java/wjhj/orbital/sportsmatchfindingapp/MainActivity.java
+++ app/src/main/java/wjhj/orbital/sportsmatchfindingapp/MainActivity.java
@@ -1,13 +1,49 @@
 package wjhj.orbital.sportsmatchfindingapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Calendar;
+
+import wjhj.orbital.sportsmatchfindingapp.database.Data;
+import wjhj.orbital.sportsmatchfindingapp.database.Game;
+import wjhj.orbital.sportsmatchfindingapp.database.User;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Data.addUser("haw_jiaa",
+                User.Builder.startBuilder()
+                        .setFirstName("hj")
+                        .setLastName("l")
+                        .setBirthday(new Date(1997, 2, 6))
+                        .setEmail("abc@hi.com")
+                        .setGender("male")
+                        .setLocation("Singapore")
+                        .setPreferences("Football")
+                        .build());
+        Data.addGame(Game.Builder.startBuilder()
+                .setEndTime(new Date(System.currentTimeMillis()))
+                .setStartTime(new Date(System.currentTimeMillis()))
+                .setMaxPlayers(5)
+                .setMinPlayers(5)
+                .setSkill("beginner")
+                .setSport("frisbee")
+                .addDetails("frisbeeeee")
+                .addLocation("nus science")
+                .addName("hahaha")
+                .addUserName("haw_jia", "haw_jiaa", "wei_jie")
+                .build());
     }
 }
---
+++ app/src/main/java/wjhj/orbital/sportsmatchfindingapp/auth/UserState.java
@@ -0,0 +1,8 @@
+package wjhj.orbital.sportsmatchfindingapp.auth;
+
+public class UserState {
+    private static final String username = "haw_jiaa";
+    public static String getUsername() {
+        return username;
+    }
+}
