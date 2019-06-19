88bfb6732ea3b79f38fd23d7f077340b25c93e90
weiiijiiie <h_weijie@yahoo.com>
2019-06-19 23:22:05 +0800

    Added functionality to login page
    - Added password/email verification, as well as error messages
    - Login button mostly works :)

--- app/src/main/java/wjhj/orbital/sportsmatchfindingapp/login/LoginActivity.java
+++ app/src/main/java/wjhj/orbital/sportsmatchfindingapp/login/LoginActivity.java
@@ -3,36 +3,37 @@ package wjhj.orbital.sportsmatchfindingapp.login;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+import javax.annotation.Nullable;
 
 import wjhj.orbital.sportsmatchfindingapp.R;
 
 public class LoginActivity extends AppCompatActivity {
     public static final String LOGIN_DEBUG = "login";
-    private static final String USERNAME_INPUT = "usernameInput";
-    private static final String PASSWORD_INPUT = "passwordInput";
+    private static final String EMAIL_INPUT_TAG = "emailInput";
+    private static final String PASSWORD_INPUT_TAG = "passwordInput";
 
     private FirebaseAuth mAuth;
-    private EditText mUsernameField;
-    private EditText mPasswordField;
+    private TextInputEditText mEmailField;
+    private TextInputEditText mPasswordField;
     private ImageButton mFacebookLogin;
     private ImageButton mGoogleLogin;
     private ImageButton mTwitterLogin;
     private TextView mSignupButton;
-    private Button mLoginButton;
+    private MaterialButton mLoginButton;
 
-    private String mUsernameInput;
-    private String mPasswordInput;
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(LOGIN_DEBUG, "Login activity created");
         super.onCreate(savedInstanceState);
@@ -40,7 +41,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
 
         mAuth = FirebaseAuth.getInstance();
-        mUsernameField = findViewById(R.id.username_field);
+        mEmailField = findViewById(R.id.email_field);
         mPasswordField = findViewById(R.id.password_field);
         mFacebookLogin = findViewById(R.id.login_facebook);
         mGoogleLogin = findViewById(R.id.login_google);
@@ -48,62 +49,32 @@ public class LoginActivity extends AppCompatActivity {
         mSignupButton = findViewById(R.id.signup_button);
         mLoginButton = findViewById(R.id.login_button);
 
-        if (savedInstanceState != null) {
-            mUsernameField.setText(savedInstanceState.getString(USERNAME_INPUT));
-            mPasswordField.setText(savedInstanceState.getString(PASSWORD_INPUT));
-        }
-
-        mUsernameField.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                mUsernameInput = s.toString();
+        mEmailField.setOnKeyListener((view, i, keyEvent) -> {
+            if (isEmailValid(mEmailField.getText())) {
+                mEmailField.setError(null);
             }
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-        mPasswordField.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                mPasswordInput = s.toString();
-            }
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-            @Override
-            public void afterTextChanged(Editable s) {}
+            return false;
         });
 
-        mFacebookLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Do something...
+        mPasswordField.setOnKeyListener((view, i, keyEvent) -> {
+            if (isPasswordValid(mPasswordField.getText())) {
+                mPasswordField.setError(null);
             }
+            return false;
         });
-        mGoogleLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Do something...
-            }
-        });
-        mTwitterLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Do something...
+
+        mLoginButton.setOnClickListener(view -> {
+            if (!isPasswordValid(mPasswordField.getText())) {
+                mPasswordField.setError("Password must be at least 8 characters!");
+            } else if (!isEmailValid(mEmailField.getText())) {
+                mEmailField.setError("Not a valid email address");
+            } else {
+                mPasswordField.setError(null);
+                trySignIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
             }
         });
 
-        mSignupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Do something...
-            }
-        });
-        mLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Do something...
-            }
+        mSignupButton.setOnClickListener(view -> {
         });
     }
 
@@ -112,14 +83,35 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
 
         FirebaseUser currUser = mAuth.getCurrentUser();
-        //WIP HELP
+
+        if (currUser != null) {
+            updateOnLoggedIn(currUser);
+        }
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putString(USERNAME_INPUT, mUsernameInput);
-        savedInstanceState.putString(PASSWORD_INPUT, mPasswordInput);
+    private boolean isEmailValid(@Nullable Editable email) {
+        return email != null && EmailValidator.getInstance().isValid(email.toString());
+    }
 
-        super.onSaveInstanceState(savedInstanceState);
+    private boolean isPasswordValid(@Nullable Editable password) {
+        return password != null && password.length() >= 8;
+    }
+
+    private void trySignIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(LOGIN_DEBUG, "sign in w email/password success");
+                        updateOnLoggedIn(mAuth.getCurrentUser());
+                    } else {
+                        Log.d(LOGIN_DEBUG, "sign in w email/password failure", task.getException());
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+    }
+
+    private void updateOnLoggedIn(FirebaseUser currUser) {
+        //navigate to homepage
     }
 }
