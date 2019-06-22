6a87db3f324d812cd48867e76f7de785e86fd2d9
Haw Jia <hawjiaa@gmail.com>
2019-06-22 18:26:41 +0800

    Added method to upload display image and return uri for it

--- app/src/main/java/wjhj/orbital/sportsmatchfindingapp/auth/SignupActivity.java
+++ app/src/main/java/wjhj/orbital/sportsmatchfindingapp/auth/SignupActivity.java
@@ -1,8 +1,20 @@
 package wjhj.orbital.sportsmatchfindingapp.auth;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -19,5 +31,21 @@ public class SignupActivity extends AppCompatActivity {
 
     private void OnSignUpClick(String displayName, String email, String password) {
 
+    }
+
+    private Task<Uri> uploadDisplayImageAndGetUri(Bitmap displayImage, String uid) {
+        ByteArrayOutputStream imageByteStream = new ByteArrayOutputStream();
+        displayImage.compress(Bitmap.CompressFormat.PNG, 100, imageByteStream);
+        byte[] imageBytes = imageByteStream.toByteArray();
+
+        StorageReference imageReference = FirebaseStorage.getInstance()
+                .getReference()
+                .child("display-images")
+                .child(uid + ".png");
+
+        UploadTask upload = imageReference.putBytes(imageBytes);
+        Task<Uri> imageUri = upload.continueWithTask(
+                (snapshot) -> imageReference.getDownloadUrl());
+        return imageUri;
     }
 }
