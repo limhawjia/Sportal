ba12f15427d73fae84d776209fe95b43387dd4d8
weiiijiiie <h_weijie@yahoo.com>
2019-07-27 03:34:55 +0800

    Merged :D

--- app/src/main/java/wjhj/orbital/sportsmatchfindingapp/homepage/gamespage/GamesCardAdapter.java
+++ app/src/main/java/wjhj/orbital/sportsmatchfindingapp/homepage/gamespage/GamesCardAdapter.java
@@ -1,11 +1,7 @@
 package wjhj.orbital.sportsmatchfindingapp.homepage.gamespage;
 
-import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -17,12 +13,11 @@ import java.util.List;
 
 import wjhj.orbital.sportsmatchfindingapp.databinding.GamesCardViewBinding;
 import wjhj.orbital.sportsmatchfindingapp.game.Game;
-import wjhj.orbital.sportsmatchfindingapp.game.GameActivity;
 
-public class GamesCardAdapter extends RecyclerView.Adapter<CardViewHolder> {
+public class GamesCardAdapter extends RecyclerView.Adapter<GamesCardAdapter.CardViewHolder> {
 
     public interface GameCardClickedListener {
-        public void onGameSelected(Game game);
+        void onGameSelected(Game game);
     }
 
     private List<Game> games;
@@ -42,7 +37,7 @@ public class GamesCardAdapter extends RecyclerView.Adapter<CardViewHolder> {
 
     @NonNull
     @Override
-    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public GamesCardAdapter.CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // create view for each data in the set
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         GamesCardViewBinding binding = GamesCardViewBinding.inflate(inflater, parent, false);
@@ -51,11 +46,9 @@ public class GamesCardAdapter extends RecyclerView.Adapter<CardViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CardViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull GamesCardAdapter.CardViewHolder holder, int position) {
         holder.setGame(games.get(position));
-        holder.itemView.setOnClickListener(view -> {
-            listener.onGameSelected(games.get(position));
-        });
+        holder.itemView.setOnClickListener(view -> listener.onGameSelected(games.get(position)));
     }
 
     @Override
